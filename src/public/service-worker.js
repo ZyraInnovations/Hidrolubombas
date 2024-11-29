1381721bc917be3e11a6c01748b0daed8376df62
julian <amayacarlos898@gmail.com>
2024-11-29 15:26:40 -0500

    app

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -1,37 +1,22 @@
-// Instalación del Service Worker y cacheo de recursos
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('app-cache').then(function(cache) {
             return cache.addAll([
-                '/login', // Página principal
-                '/login', // Ruta del login que carga la vista Handlebars
-                '/styles.css', // Archivo CSS
-                '/app.js', // Archivo JavaScript principal
+                '/',           // Página raíz, por si alguien accede directamente
+                '/login',      // Página de login
+                '/styles.css', // Archivo CSS principal
+                '/app.js',     // Archivo JavaScript principal
                 '/manifest.json', // Manifesto de la aplicación
-                '/imagenes/Recurso 1.png' // Asegúrate de que el espacio esté codificado correctamente
+                '/imagenes/Recurso 1.png' // Iconos
             ]);
         }).then(() => self.skipWaiting()) // Hace que el nuevo Service Worker tome el control inmediatamente
     );
 });
 
-// Interceptar solicitudes y responder desde el cache si es posible
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+            return response || fetch(event.request);  // Responde desde el cache o hace una nueva solicitud
         })
-    );
-});
-
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        new Promise(resolve => setTimeout(resolve, 1000)) // Retraso de 1 segundo
-        .then(() => caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
-        })).then(() => self.clients.claim())
     );
 });
