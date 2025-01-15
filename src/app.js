2aea41486dd0e4c27c0c565f78172e0b2293b8e9
julian <amayacarlos898@gmail.com>
2025-01-15 09:38:42 -0500

    cambio de base

--- src/app.js
+++ src/app.js
@@ -325,10 +325,10 @@ app.use(express.urlencoded({ extended: true })); // Esto es importante para mane
 
 // Configuración de la base de datos
 const db = mysql.createConnection({
-    host: '35.238.176.167',
+    host: '34.44.185.78',
     user: 'julian',
     password: '1034277764C',
-    database: 'cerceta',
+    database: 'hidraulibombas',
     port: 3306,
     waitForConnections: true,
     connectionLimit: 100,  // Aumentado para permitir más conexiones simultáneas si es necesario
--- src/db.js
+++ src/db.js
@@ -7,10 +7,10 @@ const mysql = require('mysql2');
 
 // Configuración del pool de conexiones
 const pool = mysql.createPool({
-    host: '35.238.176.167',
+    host: '34.44.185.78',
     user: 'julian',
     password: '1034277764C',
-    database: 'cerceta',
+    database: 'hidraulibombas',
     port: 3306,
     waitForConnections: true,
     connectionLimit: 100,  // Aumentado para permitir más conexiones simultáneas si es necesario
