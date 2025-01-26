1fa0d77ad9d508e6e889f80413c04ca2cf346050
julian <amayacarlos898@gmail.com>
2025-01-25 19:21:53 -0500

    cambio de base

--- src/db.js
+++ src/db.js
@@ -16,7 +16,7 @@ const pool = mysql.createPool({
     connectionLimit: 100,  // Aumentado para permitir más conexiones simultáneas si es necesario
     queueLimit: 0,  // Sin límite en la cola de conexiones
     connectTimeout: 5000  // Reducido a 5 segundos para intentar conexiones más rápidas
-}).promise();  // Esto convierte el pool en una versión que utiliza promesas
+}).promise();  // Esto conversión que utiliza promesas
 
 
 
